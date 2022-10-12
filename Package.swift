8fbc8cec5d291d16130ae716747b2c8b033ad3a3
Chintan Patel <chpatel@twilio.com>
2022-10-12 15:28:09 -0500

    Revert "Update for 5.3.0"
    This reverts commit 129cb83f2dd65ecccffe5b15bb6bcf9ee80ee6ed.

--- Package.swift
+++ Package.swift
@@ -1,22 +1,28 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "RELEASE_PACKAGE_NAME",
-    platforms: [
-        .iOS("12.2")
-    ],
+    name: "TwilioAudioProcessors",
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "RELEASE_PACKAGE_NAME",
-            targets: ["RELEASE_PACKAGE_NAME"]),
+            name: "TwilioAudioProcessors",
+            targets: ["TwilioAudioProcessors"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(
-            name: "RELEASE_PACKAGE_NAME",
-            url: "RELEASE_ARCHIVE_URL",
-            checksum: "RELEASE_CHECKSUM"
-        ),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "TwilioAudioProcessors",
+            dependencies: []),
+        .testTarget(
+            name: "TwilioAudioProcessorsTests",
+            dependencies: ["TwilioAudioProcessors"]),
     ]
 )
