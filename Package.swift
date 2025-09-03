60645e076cff7c248cbcb264b48afe3c8672d199
twilio-sdk-build <twilio-sdk-build@twilio.com>
2025-09-03 18:55:04 +0000

    Update for 5.10.1

--- Package.swift
+++ Package.swift
@@ -11,12 +11,20 @@ let package = Package(
         .library(
             name: "TwilioAudioProcessors",
             targets: ["TwilioAudioProcessors"]),
+        .library(
+            name: "TwilioAudioProcessors-static",
+            targets: ["TwilioAudioProcessors"])
     ],
     targets: [
         .binaryTarget(
             name: "TwilioAudioProcessors",
-            url: "https://github.com/twilio/twilio-audio-processors-ios/releases/download/5.10.0/TwilioAudioProcessors.xcframework.zip",
-            checksum: "e5cb6cc4691213b5b4426b5f71169469afbf961291685127916aaf3550f67796"
+            url: "https://github.com/twilio/twilio-audio-processors-ios/releases/download/5.10.1/TwilioAudioProcessors.xcframework.zip",
+            checksum: "93c374da48b4a7903432c5c280f011ed53543ed5105f6cd9f659a1e4d508a5f6"
         ),
+        .binaryTarget(
+            name: "TwilioAudioProcessors-static",
+            url: "https://github.com/twilio/twilio-audio-processors-ios/releases/download/5.10.1/TwilioAudioProcessors-static.xcframework.zip",
+            checksum: "2d36c81fcb69e9afee5f7ca1e55ae55e834a26a573f16c3e748416ce5bff4732"
+        )
     ]
 )
